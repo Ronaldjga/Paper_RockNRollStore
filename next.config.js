f41e117c6bd150476a95d51b5cda98e015bf220a
Ronald Almeida <Ronaldalmeida20022003@gmail.com>
2023-07-15 17:05:11 -0300

    new user in supabase with nextauth

--- next.config.js
+++ next.config.js
@@ -9,6 +9,8 @@ module.exports = {
     env: {
         SUPABASE_URL: process.env.SUPABASE_URL,
         SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+        SUPABASE_SECRET: process.env.SUPABASE_SECRET,
+        SUPABASE_JWT_SECRET: process.env.SUPABASE_JWT_SECRET,
         GITHUB_ID: process.env.GITHUB_ID,
         GITHUB_SECRET: process.env.GITHUB_SECRET,
         NEXTAUTHSECRET: process.env.NEXTAUTHSECRET
--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from 'next-auth/providers/github';
 import { SupabaseAdapter } from "@auth/supabase-adapter";
+import jwt from "jsonwebtoken"
 
 export const authOptions = {
     secret: process.env.NEXTAUTHSECRET,
@@ -12,10 +13,42 @@ export const authOptions = {
     ],
     adapter: SupabaseAdapter({
         url: process.env.SUPABASE_URL,
-        secret: process.env.SUPABASE_ANON_KEY
+        secret: process.env.SUPABASE_SECRET
     }),
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            if (user) {
+                return true
+            }
+            return false
+        },
+        async redirect({ url, baseUrl }) {
+            if (url.startsWith(baseUrl)) return url
+            else if (url.startsWith('/')) return new URL(url, baseUrl).toString()
+            return baseUrl
+        },
+        async session({ session, token, user }) {
+            if (token) {
+                session.id = token.id
+            }
+            return session
+        },
+        async jwt({ token, user, account, profile, isNewUser }) {
+            if (user) {
+                token.id = user.id
+            }
+        
+            return token
+        },
+    },
     pages: {
         signIn: "/login"
+    },
+    debug: true,
+    session: {
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60,
+        updateAge: 24 * 60 * 60,
     }
 }
 
