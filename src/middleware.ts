29b63e01da51c8ce39d54ed5475bb4e92ea81e87
Ronald Almeida <ronald.almeida@prodepa.pa.gov.br>
2023-11-30 14:10:44 -0300

    auth session token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
-    const session = req.cookies.get("next-auth.session-token")
+export async function middleware(req: NextRequest) {
+    const sessionDev = await req.cookies.get("next-auth.session-token");
+    const sessionProd = await req.cookies.get("__Secure-next-auth.session-token");
+    const session = sessionProd || sessionDev
     if((req.nextUrl.pathname.startsWith("/cart") || req.nextUrl.pathname.startsWith("/wishlist")) && !session){
-        console.log('não tem sessãoooooooooooooooooooooooooo')
         return NextResponse.redirect(new URL("/login", req.url))
     }
-    console.log('temmmmmmmmmmmmmmm sessãoooooooooooooooooooooooooooo')
     return null
 }
 
